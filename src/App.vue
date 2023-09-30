f899b42bf5712d0efcbb3063b543c7e5e7002895
andreabertanzon <andrea.bertanzon@protonmail.com>
2023-09-30 12:03:59 +0200

    transitions between routes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <AppHeader />
 
-  <router-view></router-view>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
 
   <AppPlayer />
   <AppAuth />
@@ -34,4 +38,17 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.5s linear;
+}
+
+.fade-leave-to {
+  transition: all 0.5s linear;
+  opacity: 0;
+}
+</style>
